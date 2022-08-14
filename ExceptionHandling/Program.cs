9c94b2bb81bbd1b755c9bda51af28e43a45869bc
damiansoch <66294165+damiansoch@users.noreply.github.com>
2022-08-14 10:15:49 +0100

    Add project files.

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<int> numbers = new List<int> { 81, 54, 76, 52, 43, 75, 92, 20, 43, 51 };
+                Console.WriteLine("Write the number");
+                double userInput = Convert.ToDouble(Console.ReadLine());
+
+                if (userInput == 0)
+                {
+                    Console.WriteLine("Can't divide by 0!");
+                    return;
+                }
+
+                foreach (int number in numbers)
+                {
+                    Console.WriteLine(number + " divided by " + userInput + " = " + number / userInput);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
